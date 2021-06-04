19426b0f41b861ff9ff99e2e370168541126ed08
Rafael Moral Pareja <rafael.moral@adevinta.com>
2021-06-04 17:31:31 +0200

    add ie test and add to CI

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function getImageFormats(userAgent) {
+  return {}
+}
+
+export default getImageFormats
---
+++ test/ie.test.js
@@ -0,0 +1,81 @@
+import getImageFormats from '../src'
+
+const ieVersions = {
+  6: {
+    userAgent:
+      'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)',
+    imageFormats: {
+      avif: false,
+      webp: false,
+      png: false,
+      jpg: false,
+      gif: false,
+      svg: false
+    }
+  },
+  7: {
+    userAgent:
+      'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322)',
+    imageFormats: {
+      avif: false,
+      webp: false,
+      png: true,
+      jpg: false,
+      gif: false,
+      svg: false
+    }
+  },
+  8: {
+    userAgent:
+      'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
+    imageFormats: {
+      avif: false,
+      webp: false,
+      png: true,
+      jpg: false,
+      gif: true,
+      svg: false
+    }
+  },
+  9: {
+    userAgent: 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    imageFormats: {
+      avif: false,
+      webp: false,
+      png: true,
+      jpg: true,
+      gif: true,
+      svg: true
+    }
+  },
+  10: {
+    userAgent:
+      'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    imageFormats: {
+      avif: false,
+      webp: false,
+      png: true,
+      jpg: true,
+      gif: true,
+      svg: true
+    }
+  },
+  11: {
+    userAgent: 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    imageFormats: {
+      avif: false,
+      webp: false,
+      png: true,
+      jpg: true,
+      gif: true,
+      svg: true
+    }
+  }
+}
+
+Object.keys(ieVersions).forEach(version => {
+  test(`IE ${version}`, () => {
+    const {imageFormats, userAgent} = ieVersions[version]
+    expect(getImageFormats(userAgent)).toBe(imageFormats)
+  })
+})
